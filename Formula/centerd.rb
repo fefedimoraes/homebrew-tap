29c547810dcda6895691f79d10705a21208e2af0
Fernando Ferreira Diniz de Moraes <1682707+fefedimoraes@users.noreply.github.com>
2025-08-24 01:48:36 -0500

    centerd 0.0.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/centerd.rb
+++ Formula/centerd.rb
@@ -2,9 +2,9 @@ class Centerd < Formula
   desc "Warp mouse cursor to an application window using a terminal command"
   homepage "https://github.com/fefedimoraes/centerd"
   license "MIT"
-  url "https://github.com/fefedimoraes/centerd/releases/download/v0.0.0/v0.0.0.tar.gz"
-  sha256 "placeholder"
-  version "0.0.0"
+  url "https://github.com/fefedimoraes/centerd/releases/download/v0.0.1/v0.0.1.tar.gz"
+  sha256 "cedc628cb877441627d5d908ce3263fddbec581a89e5523d3729da4c34ed4504"
+  version "0.0.1"
 
   depends_on :macos
 
